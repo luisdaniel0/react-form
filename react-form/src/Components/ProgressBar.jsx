05414c021148eb4512be4f3db918153c5b45971c
Luis Sanchez <luisanchezdh@gmail.com>
2025-10-31 15:50:57 -0400

    dynamically added current/active/incomplete classes to the progress bar as well as line connectors in each

--- react-form/src/Components/ProgressBar.jsx
+++ react-form/src/Components/ProgressBar.jsx
@@ -1,12 +1,21 @@
 const ProgressBar = ({ currentStep }) => {
   const steps = [1, 2, 3];
+
   return (
     <div className="steps">
       {steps.map((step) => {
         return (
-          <div key={step} className="step">
-            {step}
-          </div>
+          <>
+            <div
+              key={step}
+              className={`step ${currentStep > step ? "completed" : ""} ${
+                currentStep === step ? "active" : ""
+              } ${currentStep < step ? "incomplete" : ""}`}
+            >
+              {step}
+            </div>
+            {step < steps.length && <div className="connector"></div>}
+          </>
         );
       })}
     </div>
@@ -14,3 +23,6 @@ const ProgressBar = ({ currentStep }) => {
 };
 
 export default ProgressBar;
+
+//add styling depending on what step we're in the array.
+//compare currentstep to the individual step in the steps array
